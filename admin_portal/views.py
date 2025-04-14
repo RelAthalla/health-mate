dc5965897aa38e8908341b666a408cc14159ae86
lhakiim <luqmanulhakim2021@gmail.com>
2025-04-14 19:56:33 +0700

    fix doctor

--- admin_portal/views.py
+++ admin_portal/views.py
@@ -77,7 +77,7 @@ def patient_detail(request, patient_id):
     with connection.cursor() as cursor:
         cursor.execute(
             """
-            SELECT a.*, d.name as doctor_name
+            SELECT a.*, d.First_Name || ' ' || d.Last_Name AS doctor_name
             FROM appointment a
             JOIN doctor d ON a.doctor_id = d.doctor_id
             WHERE a.patient_id = %s
